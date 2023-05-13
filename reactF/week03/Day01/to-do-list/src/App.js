cc2a8d4eab455defe590b16c045b29a9fbff4862
Harshit Sachan <harshitsachan99@gmail.com>
2023-05-13 10:26:29 +0530

    done

---
+++ reactF/week03/Day01/to-do-list/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './Components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
